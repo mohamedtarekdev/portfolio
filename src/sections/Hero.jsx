0c629e9c4c3cde6be42f2df7259bfce34e2ae742
mohamedtarekabdelhamid <mohamedtarekabdelhamid@gmail.com>
2025-02-15 19:05:27 +0200

    add Hero section with background styling and NavbarComponent integration

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import '../styles/hero.css';
+
+import NavbarComponent from '../components/NavbarComponent';
+
+const Hero = () => {
+    return (
+        <div className="hero">
+            <NavbarComponent />
+        </div>
+    );
+};
+
+export default Hero;
