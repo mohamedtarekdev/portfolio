5864b2f52191d64ed0bce686464e08e160334d90
mohamedtarekabdelhamid <mohamedtarekabdelhamid@gmail.com>
2025-02-15 19:05:32 +0200

    add Hero component to App for enhanced layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import TechStack from './sections/TechStack';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import Hero from './sections/Hero';
 
 function App() {
     return (
         <>
+            <Hero />
             <TechStack />
         </>
     );
