12c5a99ed9675c427ada160a41cb876f13ae0ff9
mohamedtarekabdelhamid <mohamedtarekabdelhamid@gmail.com>
2025-02-16 18:48:58 +0200

    add Contacts section to App for improved layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
 
-import TechStack from './sections/TechStack';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+
 import Hero from './sections/Hero';
+import TechStack from './sections/TechStack';
+import Contacts from './sections/Contacts';
 
 function App() {
     return (
         <>
             <Hero />
             <TechStack />
+            <Contacts />
         </>
     );
 }
